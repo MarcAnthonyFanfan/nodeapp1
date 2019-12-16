553f3f614862b82b2867d852d577540903e195fa
MarcAnthony Fanfan <mfanx2@gmail.com>
2019-12-16 13:16:33 -0500

    Attempting to connect to mysql

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var http = require('http');
+var mysql = require('mysql');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
 }).listen(8080);
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+  
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 console.log('Server started');
