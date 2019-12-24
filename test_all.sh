b490b9ee05e3c96756eefe3412365d90ce1bafee
MarcAnthony Fanfan <mfanx2@gmail.com>
2019-12-24 11:07:37 -0500

    Finished basic test script

--- test_all.sh
+++ test_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo 'Testing Node.js Application'
 echo 'Platforms: [Linux, Windows]'
-echo 'Browsers: [Chrome, Firefox]'
+printf 'Browsers: [Chrome, Firefox]\n\n'
 # Platform: Linux
-GRID_BROWSER='chrome' GRID_PLATFORM='linux' python3 test/grid_test.py > test/windows_chrome_output.txt 2>&1
-GRID_BROWSER='firefox' GRID_PLATFORM='linux' python3 test/grid_test.py > test/windows_firefox_output.txt 2>&1
+GRID_BROWSER='chrome' GRID_PLATFORM='linux' python3 test/grid_test.py > test/linux_chrome_output.txt 2>&1 && printf '   [Linux/Chrome] '; grep 'tests passed' test/linux_chrome_output.txt
+GRID_BROWSER='firefox' GRID_PLATFORM='linux' python3 test/grid_test.py > test/linux_firefox_output.txt 2>&1 && printf '  [Linux/Firefox] '; grep 'tests passed' test/linux_firefox_output.txt
 # Platform: Windows
-GRID_BROWSER='chrome' GRID_PLATFORM='windows' python3 test/grid_test.py > test/windows_chrome_output.txt 2>&1
-GRID_BROWSER='firefox' GRID_PLATFORM='windows' python3 test/grid_test.py > test/windows_firefox_output.txt 2>&1
+GRID_BROWSER='chrome' GRID_PLATFORM='windows' python3 test/grid_test.py > test/windows_chrome_output.txt 2>&1 && printf ' [Windows/Chrome] '; grep 'tests passed' test/windows_chrome_output.txt
+GRID_BROWSER='firefox' GRID_PLATFORM='windows' python3 test/grid_test.py > test/windows_firefox_output.txt 2>&1 && printf '[Windows/Firefox] '; grep 'tests passed' test/windows_firefox_output.txt
