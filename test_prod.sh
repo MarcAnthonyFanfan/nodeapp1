c570e3dee741e6e3e35997b17f0eb55d0f157ccc
MarcAnthony Fanfan <mfanx2@gmail.com>
2020-01-08 13:00:51 -0500

    Print when tests fail to execute

--- test_prod.sh
+++ test_prod.sh
@@ -9,7 +9,15 @@ printf 'Browsers: [Chrome, Firefox]\n\n'
 ./test/prod/windows_chrome_test.sh & PID_WINDOWS_CHROME=$!
 ./test/prod/windows_firefox_test.sh & PID_WINDOWS_FIREFOX=$!
 # Wait for test results
-wait $PID_LINUX_CHROME
-wait $PID_LINUX_FIREFOX
-wait $PID_WINDOWS_CHROME
-wait $PID_WINDOWS_FIREFOX
+FAILED_TO_EXECUTE=0
+wait $PID_LINUX_CHROME || FAILED_TO_EXECUTE=$((FAILED_TO_EXECUTE+1))
+wait $PID_LINUX_FIREFOX || FAILED_TO_EXECUTE=$((FAILED_TO_EXECUTE+1))
+wait $PID_WINDOWS_CHROME || FAILED_TO_EXECUTE=$((FAILED_TO_EXECUTE+1))
+wait $PID_WINDOWS_FIREFOX || FAILED_TO_EXECUTE=$((FAILED_TO_EXECUTE+1))
+if [ "$FAILED_TO_EXECUTE" -eq "0" ]
+then
+   exit 0
+else
+    printf "\n$FAILED_TO_EXECUTE Tests Failed to Execute\n"
+    exit 1
+fi
